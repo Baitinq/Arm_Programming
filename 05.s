87f86ba6217c4dfdcbc7b31885b491301be832c9
Manuel Palenzuela <manuelpalenzuelamerino@gmail.com>
2019-10-16 19:19:31 +0100

    Finish exercise 5

--- 05.s
+++ 05.s
@@ -1,59 +1,90 @@
-        B start
+        B setvar
 
 
-verse	DEFW	4
-menmow  DEFB 	"went to mow\n",0
+verse	DEFW	2
+menmow  DEFB 	" went to mow\n",0
 mowdow  DEFB    "Went to mow a meadow\n",0
 men     DEFB    "men",0
 man     DEFB    "man",0
 comma   DEFB    ",",0
 nl      DEFB    "\n",0
 space   DEFB    " ",0
-and_dog DEFB    "and his dog, Spot",0
+and_dog DEFB    "and his dog, Spot.",0
 
         
         ALIGN
-start   MOV R1, #0
-        MOV R2, #4
+setvar  MOV R1, #0
+        ADR R2, 5
 
-;4 men went to mow
 main    SUB R2, R2, #1
-	ADR R0, R2
+
+        CMP R2, #0
+
+        BGT versemn
+
+        SWI 2
+
+
+versemn MOV R0, R2
+        SWI 4
+        ADR R0, space
+        SWI 3
+        
+        CMP R2, #1
+        BEQ sglrvr
+        
+        ADR R0, men ;
+        SWI 3
+
+contvr  ADR R0, menmow
+        SWI 3
+        
+        ADR R0, mowdow
+        SWI 3
+        
+        MOV R3, R2
+        B verselp
+
+contin  ADR R0, nl
+        SWI 3
+        ADR R0, mowdow 
+        SWI 3
+        ADR R0, nl
+        SWI 3
+
+        B main
+
+verselp MOV R0, R3
+        SWI 4
+        ADR R0, space
+        SWI 3
+        
+        CMP R3, #1
+        BEQ singlr
+
+        ADR R0, men
+        SWI 3
+        ADR R0, comma
+        SWI 3
+
+contver ADR R0, space
+        SWI 3
+
+        SUB R3, R3, #1
+        
+        CMP R3, #0
+        BEQ contin
+        B verselp
+
+singlr  ADR R0, man
         SWI 3
         ADR R0, space
         SWI 3
-        ADR R0, men
+        ADR R0, and_dog
         SWI 3
-        ADR R0, menmow
+    
+        B contver
 
-;Went to mow a meadow\n
-        ADR R0, mowdow
+sglrvr  ADR R0, man
         SWI 3
-
-;4 men, X men,
-        ; R2 is the number from 4 to 1
-        ADR R0, R2
-        SUB R0, R0, R1
-        CMP R0, #1 ;i need 2 counters
-        ADD R1, R1, #1 ;increase the substract value
-        SWI 4
-       	 
-        BEQ singlr
-
-	ADR R0, men
-	SWI 3
-        
-        B main
-
-        ADR R0, mowdow
-        SWI 3
-        
-        CMP R2, #4
-        BST main
-
-        SWI 2  
-
-singlr  ADR R0, man
-	SWI 3
-	
-	B main
+        B contvr
